5b6431bedbe816dcb98e821daba46007cbbfcdc6
gary <gary@hazardbrick.com>
2013-09-03 23:04:51 -0700

    fixed / error. fixed getLastFmData, fixed getTopTags

--- modules/lastfm.php
+++ modules/lastfm.php
@@ -94,7 +94,7 @@ function getLastfmData($method, $parameters) {
 
 	if (in_array("internets", $loaded_modules)) {
 		$message = @internets_get_contents('http://ws.audioscrobbler.com/2.0/?format=json&api_key='. LASTFM_API_KEY .'&method=' . $method . '&' . $parameters);
-		$message = json_decode($def, TRUE);
+		$message = json_decode($message, TRUE);
 	} else {
 		send_msg($channel, "This will not work without the internets module.");
 	}
@@ -104,22 +104,23 @@ function getLastfmData($method, $parameters) {
 
 function getTopTags($artist) {
 	$tags = getLastfmData('artist.gettoptags','artist=' . urlencode($artist));
-	$tag_amount = 0;
-	$top_tags= array();
-	if ($tags['toptags']) {
-		if (isset($tags['toptags']['tag']['name'])) {
-			$top_tags = $tags['toptags']['tag']['name'];
-		} else {
-			foreach ($tags['toptags']['tag'] as $tag) {
-				if ($tag_amount++ == 5) {
-					break;
-				}
-				array_push($top_tags, $tag['name']);
+	
+	$top_tags = "";
+	
+	if(count($tags["toptags"]["tag"]) > 2) {
+		for($i = 0; $i < 3; $i++) {
+			if($i < 2) {
+				$top_tags .= $tags["toptags"]["tag"][$i]["name"] . ", ";
 			}
-			$top_tags = join(', ', $tags);
+			else {
+				$top_tags .= $tags["toptags"]["tag"][$i]["name"];
+			}
 		}
-	} else {
-		$top_tags = false;
+	}
+	else {
+		foreach($tags["toptags"]["tag"] as $tag) {
+			$top_tags .= $tag["name"];
+		}
 	}
 
 	return $top_tags;
@@ -141,7 +142,7 @@ function getNowPlaying() {
 	}
 	$json_data 	= getLastfmData('user.getrecenttracks','user=' . urlencode($user) . '&limit=1');
 	$first_track 	= $json_data['recenttracks']['track'][0];
-	if ($first_track['@attr']['nowplaying']) {/
+	if ($first_track['@attr']['nowplaying']) {
 		$trackinfo	= getLastfmData('track.getinfo','artist=' . urlencode($first_track['artist']['#text']) . '&track=' . urlencode($first_track['name']) . '&username=' . urlencode($user));
 		$top_tags 	= getTopTags($first_track['artist']['#text']);
 		$message 	=  ' "' . $user . '" is now playing '.$first_track['artist']['#text'];
